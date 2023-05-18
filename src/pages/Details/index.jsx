cda36a38844e29e86a5813b9f4a0b759ce21e93b
gabriel <gabrielbarroncas1@gmail.com>
2023-05-18 10:53:30 +0100

    create section for links

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,13 +1,24 @@
-import { Container } from "./style"
+import { Container, Links } from "./style"
 
 import { Button } from "../../components/Button"
 import { Header } from "../../components/Header"
+import { Section } from "../../components/Section"
 
 
 export function Details(){
   return (
     <Container>
-     <Header />
+      <Header />
+
+      <Section title="Links úteis">
+        <Links>
+          <li><a href="#">Link 1</a></li>
+          <li><a href="#">Link 2</a></li>
+          <li><a href="#">Link 3</a></li>
+          
+        </Links>
+
+      </Section>
 
       <Button title="Voltar" />  
     </Container>
