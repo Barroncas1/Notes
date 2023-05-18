fc9504a6232f39510a2c1b0546831eef3983b9f5
gabriel <gabrielbarroncas1@gmail.com>
2023-05-18 11:30:30 +0100

    finishing interface of details

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Links } from "./style"
+import { Container, Links, Content } from "./style"
 
 import { Button } from "../../components/Button"
 import { Header } from "../../components/Header"
@@ -11,24 +11,34 @@ export function Details(){
   return (
     <Container>
       <Header />
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota" />
+          <h1>
+            Introdução ao React
+          </h1>
 
-      <ButtonText title="Excluir nota" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel impedit officia officiis laborum optio nulla perspiciatis eos neque nisi tempora at iusto veniam, iure saepe nostrum eaque dolorum ratione adipisci! Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit labore animi, a eaque molestiae et perferendis aperiam excepturi explicabo qui maiores dicta suscipit quas, amet nihil consectetur error vel delectus?
+          </p>
 
-      <Section title="Links úteis">
-        <Links>
-          <li><a href="#">Link 1</a></li>
-          <li><a href="#">Link 2</a></li>
-          <li><a href="#">Link 3</a></li>
-        </Links>
-      </Section>
+          <Section title="Links úteis">
+            <Links>
+              <li><a href="#">https://github.com/barroncas1</a></li>
+              <li><a href="#">https://github.com/barroncas1</a></li>
+              <li><a href="#">https://github.com/barroncas1</a></li>
+            </Links>
+          </Section>
 
-      <Section title="Marcadores">
-        <Tag title="express" />
-        <Tag title="node" />
-        <Tag title="react" />
-      </Section>
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="node" />
+            <Tag title="react" />
+          </Section>
 
-      <Button title="Voltar" />  
+          <Button title="Voltar" />
+        </Content>
+      </main>  
     </Container>
   )
 }
