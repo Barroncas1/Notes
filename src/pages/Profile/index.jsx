6a1e32633444c4befe491a3e84b879a1d6d74936
Gabriel <gabrielbarroncas1@gmail.com>
2023-06-28 15:59:06 -0300

    finish the profile

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -3,6 +3,9 @@ import { FiArrowLeft, FiMail, FiUser, FiLock, FiCamera } from "react-icons/fi";
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 import { Link } from "react-router-dom";
+import avatarPlaceHolder from "../../assets/avatar_placeholder.svg"
+import {api} from "../../services/api"
+
 import { useState } from "react";
 import { useAuth } from "../../hooks/auth";
 
@@ -14,7 +17,9 @@ export function Profile(){
     const [passwordOld, setPasswordOld] = useState()
     const [passwordNew, setPasswordNew] = useState()
 
-    const [avatar, setAvatar] = useState(user.avatar)
+
+    const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}`: avatarPlaceHolder
+    const [avatar, setAvatar] = useState(avatarUrl)
     const [avatarFile, setAvatarFile] = useState(null)
     
     async function handleUpdate(){
